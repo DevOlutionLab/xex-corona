d18645ee796a101eafdda360067c430e900e9f6d
daooda <davinhoba9@outlook.fr>
2020-04-29 19:28:51 +0000

    updating HomeComponent display ✔

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import { StatsService } from '../stats.service';
+import { TotalService } from '../total.service';
+import { MainService } from '../main.service';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,15 @@ export class HomeComponent implements OnInit {
 
   faQuoteLeft = faQuoteLeft;
 
+  public main = [];
   public stats = [];
-
-  constructor(private _statsService: StatsService) {}
+  public total = [];
+  
+  constructor(private _mainService: MainService,private _statsService: StatsService, private _totalService: TotalService) {}
 
   ngOnInit() {
     this._statsService.getStats().subscribe(data => (this.stats = data));
+    this._totalService.getTotal().subscribe(data => (this.total = data));
+    this._mainService.getMain().subscribe(data => (this.main = data));
   }
 }

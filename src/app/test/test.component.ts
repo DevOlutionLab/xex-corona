6a3f4a20220460b1f8f6b51b2061d3a6f0544812
daooda <davinhoba9@outlook.fr>
2020-05-07 06:15:44 +0000

    adding test function on TestComponent

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-test',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  testForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.testForm = this.fb.group({  // Crée une instance de FormGroup
+      // Crée une instance de FormControl
+      malDeTete: [],
+      toux: [],
+      gorgeIrritee: [],
+      fievre: [],
+      sentimentGeneralDeMalaise: [],
+      geneRespiratoire: [],
+      fatigueInnabituelle: [],
+      alimentationDifficile: [],
+      courbature: [],
+      voyage: [],
+      enContact: [],
+    });
+  }
+
+  count: number=0;
+  value;
+
+  testing() {
+
+    Object.keys(this.testForm.value)
+      .forEach((key: string) => {
+        this.value = this.testForm.get(key);
+          if (this.value.value == true) {
+            this.count++;
+            // alert(this.count)
+          }
+    })
+
+    if (this.count <= 11 && this.count >= 6) {
+      alert('Suspect!');
+      console.log(this.count, this.value)
+    }
+    else {
+      alert('Ca va!');
+      console.log(this.count)
+    }
   }
 
 }
